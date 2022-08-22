8181f5eecd7d3a85c6cacf4d667afc5f41bc05c0
David Daniel <davedan7@gmail.com>
2022-08-21 23:48:08 -0600

    chore: very minor tweaks - comparing build to mantine

--- src/components/atoms/MuiReExports.tsx
+++ src/components/atoms/MuiReExports.tsx
@@ -1,45 +1,66 @@
+export {
+  Alert,
+  AlertTitle,
+  AppBar,
+  Button,
+  Card,
+  CardContent,
+  CircularProgress,
+  FormControl,
+  IconButton,
+  InputLabel,
+  MenuItem,
+  Paper,
+  Stack,
+  Step,
+  StepLabel,
+  Stepper,
+  Typography as Text,
+  TextField,
+  Toolbar,
+} from '@mui/material'
 // re-export of MUI components to aggregate
-import MuiAlert from '@mui/material/Alert'
-import MuiAlertTitle from '@mui/material/AlertTitle'
-import MuiAppBar from '@mui/material/AppBar'
-import MuiButton from '@mui/material/Button'
-import MuiCard from '@mui/material/Card'
-import MuiCardContent from '@mui/material/CardContent'
-import MuiCircularProgress from '@mui/material/CircularProgress'
-import MuiFormControl from '@mui/material/FormControl'
-import MuiIconButton from '@mui/material/IconButton'
-import MuiInputLabel from '@mui/material/InputLabel'
-import MuiMenuItem from '@mui/material/MenuItem'
-import MuiPaper from '@mui/material/Paper'
-import MuiStep from '@mui/material/Step'
-import MuiStepLabel from '@mui/material/StepLabel'
-import MuiStepper from '@mui/material/Stepper'
-import MuiTextField from '@mui/material/TextField'
-import MuiToolbar from '@mui/material/Toolbar'
-import MuiTypography from '@mui/material/Typography'
+// import MuiAlert from '@mui/material/Alert'
+// import MuiAlertTitle from '@mui/material/AlertTitle'
+// import MuiAppBar from '@mui/material/AppBar'
+// import MuiButton from '@mui/material/Button'
+// import MuiCard from '@mui/material/Card'
+// import MuiCardContent from '@mui/material/CardContent'
+// import MuiCircularProgress from '@mui/material/CircularProgress'
+// import MuiFormControl from '@mui/material/FormControl'
+// import MuiIconButton from '@mui/material/IconButton'
+// import MuiInputLabel from '@mui/material/InputLabel'
+// import MuiMenuItem from '@mui/material/MenuItem'
+// import MuiPaper from '@mui/material/Paper'
+// import MuiStep from '@mui/material/Step'
+// import MuiStepLabel from '@mui/material/StepLabel'
+// import MuiStepper from '@mui/material/Stepper'
+// import MuiTextField from '@mui/material/TextField'
+// import MuiToolbar from '@mui/material/Toolbar'
+// import MuiTypography from '@mui/material/Typography'
 
 // TODO: the following syntax isn't ideal and might interfere with code
 // splitting, but proper syntax, IE:
 //      `export { default as Button } from '@mui/material/Button'`
 // doesn't work with intellisense... leaving like this for now but should check
 // bundle size on build to make sure splitting is working
-export { MuiTypography as Text } // shorter naming
-export const Alert = MuiAlert
-export const AlertTitle = MuiAlertTitle
-export const AppBar = MuiAppBar
-export const Button = MuiButton
-export const Card = MuiCard
-export const CardContent = MuiCardContent
-export const CircularProgress = MuiCircularProgress
-export const FormControl = MuiFormControl
-export const IconButton = MuiIconButton
-export const InputLabel = MuiInputLabel
-export const MenuItem = MuiMenuItem
-export const Paper = MuiPaper
-export const Stepper = MuiStepper
-export const Step = MuiStep
-export const StepLabel = MuiStepLabel
-export const TextField = MuiTextField
-export const Toolbar = MuiToolbar
+// export { MuiTypography as Text } // shorter naming
+// export const Alert = MuiAlert
+// export const AlertTitle = MuiAlertTitle
+// export const AppBar = MuiAppBar
+// export const Button = MuiButton
+// export const Card = MuiCard
+// export const CardContent = MuiCardContent
+// export const CircularProgress = MuiCircularProgress
+// export const FormControl = MuiFormControl
+// export const IconButton = MuiIconButton
+// export const InputLabel = MuiInputLabel
+// export const MenuItem = MuiMenuItem
+// export const Paper = MuiPaper
+// export const Stepper = MuiStepper
+// export const Step = MuiStep
+// export const StepLabel = MuiStepLabel
+// export const TextField = MuiTextField
+// export const Toolbar = MuiToolbar
 
 export type { AlertProps, SelectChangeEvent } from '@mui/material'
--- src/components/templates/AppTemplate.tsx
+++ src/components/templates/AppTemplate.tsx
@@ -1,19 +1,19 @@
 import { Suspense } from 'react'
 import { Outlet } from 'react-router-dom'
 
-import { Box, FlexCol } from '@/atoms'
+import { Box, Stack } from '@/atoms'
 import { Spinner } from '@/molecules'
 import { Navbar } from '@/organisms'
 
 export const AppTemplate = () => {
   return (
-    <FlexCol sx={{ maxHeight: '100vh', height: '100%', overflowY: 'auto' }}>
+    <Stack sx={{ maxHeight: '100vh', height: '100%', overflowY: 'auto' }}>
       <Navbar />
       <Box component="main" sx={{ height: '100%' }}>
         <Suspense fallback={<Spinner />}>
           <Outlet />
         </Suspense>
       </Box>
-    </FlexCol>
+    </Stack>
   )
 }
