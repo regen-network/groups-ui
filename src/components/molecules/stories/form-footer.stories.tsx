a8d555dae9dd3fecaefc113dae684f6a26acf4a8
David Daniel <davedan7@gmail.com>
2023-04-18 22:57:56 -0600

    chore: upgrade jotai, fix story issue

--- src/components/molecules/stories/form-footer.stories.tsx
+++ src/components/molecules/stories/form-footer.stories.tsx
@@ -1,7 +1,18 @@
 import type { Meta, StoryFn } from '@storybook/react'
-import { Provider } from 'jotai'
+import { createStore, Provider } from 'jotai'
 
 import { FormFooter, FormFooterAtom } from '../form-footer'
+
+const store = createStore()
+store.set(FormFooterAtom, {})
+
+const voidFunc = () => void null
+
+const storeWithBackBtns = createStore()
+storeWithBackBtns.set(FormFooterAtom, { onPrev: voidFunc })
+
+const storeWithBothBtns = createStore()
+storeWithBothBtns.set(FormFooterAtom, { onPrev: voidFunc, onNext: voidFunc })
 
 export default {
   title: 'Molecules/FormFooter',
@@ -15,35 +26,15 @@ export default {
 
 const Template: StoryFn<typeof FormFooter> = () => <FormFooter />
 
-const voidFunc = () => void null
-
-const baseState = {}
-
 export const Component = Template.bind({})
-Component.decorators = [
-  (Story) => (
-    <Provider initialValues={[[FormFooterAtom, baseState]]}>{<Story />}</Provider>
-  ),
-]
+Component.decorators = [(Story) => <Provider store={store}>{<Story />}</Provider>]
 
 export const WithBackBtn = Template.bind({})
 WithBackBtn.decorators = [
-  (Story) => (
-    <Provider initialValues={[[FormFooterAtom, { ...baseState, onPrev: voidFunc }]]}>
-      {<Story />}
-    </Provider>
-  ),
+  (Story) => <Provider store={storeWithBackBtns}>{<Story />}</Provider>,
 ]
 
 export const WithBothBtns = Template.bind({})
 WithBothBtns.decorators = [
-  (Story) => (
-    <Provider
-      initialValues={[
-        [FormFooterAtom, { ...baseState, onPrev: voidFunc, onNext: voidFunc }],
-      ]}
-    >
-      {<Story />}
-    </Provider>
-  ),
+  (Story) => <Provider store={storeWithBothBtns}>{<Story />}</Provider>,
 ]
