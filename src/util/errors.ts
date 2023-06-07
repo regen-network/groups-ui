9356484ca8a3dd8bee4780b6ea2e667ee24b7434
Marie Gauthier <marie.gauthier63@gmail.com>
2023-06-07 20:04:12 +0200

    fix: handle not found errors (#83)
    Co-authored-by: Ryan Christoffersen <12519942+ryanchristo@users.noreply.github.com>

--- src/util/errors.ts
+++ src/util/errors.ts
@@ -2,7 +2,7 @@ type ErrorWithMessage = {
   message: string
 }
 
-function isErrorWithMessage(error: unknown): error is ErrorWithMessage {
+export function isErrorWithMessage(error: unknown): error is ErrorWithMessage {
   return (
     typeof error === 'object' &&
     error !== null &&
