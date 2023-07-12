3c2ef77678765c4968ab1ee329b71593b386356b
Kyle Lawlor-Bagcal <kyle.lawlor@regen.network>
2023-07-12 15:55:38 -0400

    feat: add react-query devtools

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { ReactQueryProvider } from 'react-query-provider'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { ThemeProvider } from 'theme'
 
 import App from './App'
@@ -13,6 +14,7 @@ const root = createRoot(container as HTMLElement)
 root.render(
   <React.StrictMode>
     <ReactQueryProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
       <ThemeProvider>
         <App />
       </ThemeProvider>
