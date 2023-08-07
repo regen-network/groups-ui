86ef485ccd8066e5beb2c0e2de48679bdf20c260
ryanchristo <12519942+ryanchristo@users.noreply.github.com>
2023-08-07 10:23:37 -0700

    feat: add execute proposal to proposal page

--- src/pages/proposal-page.tsx
+++ src/pages/proposal-page.tsx
@@ -1,10 +1,12 @@
+import { useState } from 'react'
 import { redirect, useParams } from 'react-router-dom'
+import Long from 'long'
 
 import { VoteOptionType } from 'types'
 import { throwError } from 'util/errors'
 
 import { ROUTE_PATH } from 'routes'
-import { voteOnProposal } from 'api/proposal.actions'
+import { executeProposal, voteOnProposal } from 'api/proposal.actions'
 import {
   useGroup,
   useGroupMembers,
@@ -15,10 +17,17 @@ import {
 import { useTxToasts } from 'hooks/use-toasts'
 
 import { Button, PageContainer, RouteLink, Stack } from '@/atoms'
+import {
+  FormFooter,
+  FormSubmitHiddenButton,
+  useFormFooter,
+} from '@/molecules/form-footer'
 import { Loading } from '@/molecules/loading'
 import { ProposalDetails } from '@/organisms/proposal-details'
 import { ProposalSummary } from '@/organisms/proposal-summary'
 import { ProposalVotesTable } from '@/organisms/proposal-votes-table'
+
+import { ProposalStatus } from '../util/enums'
 
 import { BackIcon } from 'assets/tsx'
 
@@ -27,12 +36,23 @@ export default function ProposalPage() {
   const { proposalId, groupId } = useParams()
   const { data: group, isLoading: isLoadingGroup } = useGroup(groupId)
   const { data: groupMembers } = useGroupMembers(groupId)
-  const { data: proposal, isLoading: isLoadingProposal } = useProposal(proposalId)
+  const {
+    data: proposal,
+    isLoading: isLoadingProposal,
+    refetch: refetchProposal,
+  } = useProposal(proposalId)
   const {
     data: votes,
     isLoading: isLoadingVotes,
     refetch: refetchVotes,
   } = useProposalVotes(proposalId)
+  const [submitting, setSubmitting] = useState(false)
+
+  useFormFooter({
+    onNext: undefined,
+    onPrev: undefined,
+    btnText: 'Execute',
+  })
 
   const {
     data: userVotes,
@@ -59,33 +79,59 @@ export default function ProposalPage() {
     }
   }
 
+  async function handleExec(event) {
+    event.preventDefault()
+    setSubmitting(true)
+    if (!proposalId) throwError('Proposal ID is required to execute proposal')
+    try {
+      const { transactionHash } = await executeProposal({
+        proposalId: Long.fromString(proposalId),
+      })
+      toastSuccess(transactionHash)
+      refetchProposal()
+    } catch (err) {
+      toastErr(err)
+    }
+    setSubmitting(false)
+  }
+
   const userVote = userVotes
     ? userVotes.find((v) => v.proposalId.toString() === proposalId)
     : undefined
 
+  const isExecutable = proposal.status === ProposalStatus.PROPOSAL_STATUS_ACCEPTED
+
   return (
-    <PageContainer>
-      <Stack w="full" spacing={6}>
-        <div>
-          <Button
-            variant="ghost"
-            leftIcon={<BackIcon />}
-            as={RouteLink}
-            to={ROUTE_PATH.group(groupId)}
-          >
-            {group?.metadata.name}
-          </Button>
-        </div>
-        <ProposalSummary
-          proposal={proposal}
-          group={group}
-          onVote={handleVote}
-          userVote={userVote}
-          votes={votes}
-        />
-        <ProposalDetails proposal={proposal} />
-        <ProposalVotesTable votes={votes || []} groupMembers={groupMembers || []} />
-      </Stack>
-    </PageContainer>
+    <>
+      <PageContainer>
+        <Stack w="full" spacing={6}>
+          <div>
+            <Button
+              variant="ghost"
+              leftIcon={<BackIcon />}
+              as={RouteLink}
+              to={ROUTE_PATH.group(groupId)}
+            >
+              {group?.metadata.name}
+            </Button>
+          </div>
+          <ProposalSummary
+            proposal={proposal}
+            group={group}
+            onVote={handleVote}
+            userVote={userVote}
+            votes={votes}
+          />
+          <ProposalDetails proposal={proposal} />
+          <ProposalVotesTable votes={votes || []} groupMembers={groupMembers || []} />
+        </Stack>
+      </PageContainer>
+      {isExecutable && (
+        <form onSubmit={handleExec}>
+          <FormSubmitHiddenButton id="proposal-exec" onSubmit={handleExec} />
+        </form>
+      )}
+      <FormFooter isSubmitting={submitting} />
+    </>
   )
 }
