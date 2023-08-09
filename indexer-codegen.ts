007e0310ef4e849ee00b30029b4bd12b50815443
Ryan Christoffersen <12519942+ryanchristo@users.noreply.github.com>
2023-08-09 15:27:19 -0700

    chore: update regen server api endpoints (#138)
    * chore: update regen server api endpoints
    
    * empty commit

--- indexer-codegen.ts
+++ indexer-codegen.ts
@@ -2,7 +2,7 @@ import { CodegenConfig } from '@graphql-codegen/cli'
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: 'http://localhost:5000/indexer/graphql',
+  schema: 'http://localhost:5000/indexer/v1/graphql',
   documents: ['src/graphql/indexer/**/*.graphql'],
   generates: {
     './src/gql/': {
